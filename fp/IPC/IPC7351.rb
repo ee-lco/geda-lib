142e0ac4e8e36b1c2c41bf3491e8ba28713c4288
Eelco van der Werff <eelco@users.sourceforge.net>
2015-03-24 00:04:03 +0100

    Remove scripts, moved to geda-tools.git

--- fp/IPC/IPC7351.rb
+++
@@ -1,7 +0,0 @@
-['Settings', 'Environments',
- 'MinMax', 'Transformable', 'Geometry', 'Path', 'Drawable',
- 'Pads', 'Layer', 'Footprint',
- 'BaseFootprints', 'Footprints',
- 'Renderer', 'GedaPCB', 'SVG',
-].each { |file| require_relative 'IPC7351/%s' % [file] }
-
